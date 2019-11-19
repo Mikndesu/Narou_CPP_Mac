fce1c1b3168e892882087c054ea64813a3fa6dfc
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2019-11-19 18:04:14 +0900

    Initial Commit

---
+++ Narou/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Narou
+//
+//  Created by MitsukiGoto on 2019/11/19.
+//  Copyright © 2019 五島充輝. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
