07837a91d0f378011f32e32ab877bfe66ec729db
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2019-12-05 01:21:02 +0900

    IBConnection With PreferWindow.swift

--- Narou/AppDelegate.swift
+++ Narou/AppDelegate.swift
@@ -13,19 +13,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     let statusItem = NSStatusBar.system.statusItem(withLength: -1)
     var timer = Timer()
-
+    
     @IBOutlet weak var menu: NSMenu!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        let menu = NSMenu()
         self.statusItem.title = "Narou"
         self.statusItem.highlightMode = true
         self.statusItem.menu = menu
-        
-        let PreferItem = NSMenuItem()
-        PreferItem.title = "Preferences"
-        PreferItem.action = #selector(AppDelegate.preferences(_:))
-        menu.addItem(PreferItem)
         
         let reloadItem = NSMenuItem()
         reloadItem.title = "Reload"
@@ -73,9 +68,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         notification.soundName = NSUserNotificationDefaultSoundName
         let notificationCenter = NSUserNotificationCenter.default
         notificationCenter.deliver(notification)
-    }
-    
-    @IBAction func preferences(_ sender: Any) {
     }
     
     @IBAction func reload(_ sender: Any) {
---
+++ Narou/PreferWindow.swift
@@ -0,0 +1,18 @@
+//
+//  PreferWindow.swift
+//  Narou
+//
+//  Created by MitsukiGoto on 2019/12/05.
+//  Copyright © 2019 五島充輝. All rights reserved.
+//
+
+import Cocoa
+
+class PreferWindow: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
